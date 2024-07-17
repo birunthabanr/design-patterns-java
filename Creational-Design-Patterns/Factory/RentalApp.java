97a170fb60bec5b37ca6b2bd1e8bf94643cd8e12
birunthabanr <birunthur@gmail.com>
2024-07-18 00:00:01 +0530

    nothing major

--- Creational-Design-Patterns/Factory/RentalApp.java
+++ Creational-Design-Patterns/Factory/RentalApp.java
@@ -48,17 +48,60 @@ class TruckRentalService extends VehicleRentalService {
     }
 }
 
-// Client code
-public class RentalApp {
-    public static void rentVehicle(VehicleRentalService rentalService) {
-        Vehicle vehicle = rentalService.createVehicle();
-        vehicle.rent();
-    }
+// // Client code
+// public class RentalApp {
+//     public static void rentVehicle(VehicleRentalService rentalService) {
+//         Vehicle vehicle = rentalService.createVehicle();
+//         vehicle.rent();
+//     }
 
-    public static void main(String[] args) {
-        System.out.println("Using Factory Method Pattern:");
-        rentVehicle(new CarRentalService());
-        rentVehicle(new MotorcycleRentalService());
-        rentVehicle(new TruckRentalService());
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Using Factory Method Pattern:");
+//         rentVehicle(new CarRentalService());
+//         rentVehicle(new MotorcycleRentalService());
+//         rentVehicle(new TruckRentalService());
+//     }
+// }
+
+
+// // Product interface
+// interface Vehicle {
+//     void rent();
+// }
+
+// // Concrete products
+// class Car implements Vehicle {
+//     @Override
+//     public void rent() {
+//         System.out.println("Renting Car");
+//     }
+// }
+// class Motorcycle implements Vehicle {
+//     @Override
+//     public void rent() {
+//         System.out.println("Renting Motorcycle");
+//     }
+// }
+
+// class Truck implements Vehicle {
+//     @Override
+//     public void rent() {
+//         System.out.println("Renting Truck");
+//     }
+// }
+
+// // Client code without using factory method
+// public class RentalAppWithoutFactory {
+//     public static void main(String[] args) {
+//         // Without factory method
+//         Vehicle car = new Car();
+//         Vehicle motorcycle = new Motorcycle();
+//         Vehicle truck = new Truck();
+
+//         // Client code uses concrete objects directly
+//         System.out.println("Without Using Factory Method:");
+//         car.rent();
+//         motorcycle.rent();
+//         truck.rent();
+//     }
+// }
