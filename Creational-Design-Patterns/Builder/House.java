deadf667ff11f0c1d4c6dfab14b75dd82295ce6d
birunthabanr <birunthur@gmail.com>
2024-08-01 23:52:59 +0530

    -

--- Creational-Design-Patterns/Builder/House.java
+++ Creational-Design-Patterns/Builder/House.java
@@ -1,11 +1,46 @@
-class House {
+public class House {
     private int walls;
     private int doors;
     private int windows;
     private String roof;
 
-    // Getters and toString() method
+    // Private constructor to prevent direct instantiation
+    private House(Builder builder) {
+        this.walls = builder.walls;
+        this.doors = builder.doors;
+        this.windows = builder.windows;
+        this.roof = builder.roof;
+    }
 
+    // Getters for each field
+    public int getWalls() {
+        return walls;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getWindows() {
+        return windows;
+    }
+
+    public String getRoof() {
+        return roof;
+    }
+
+    // toString() method for easy representation
+    @Override
+    public String toString() {
+        return "House{" +
+                "walls=" + walls +
+                ", doors=" + doors +
+                ", windows=" + windows +
+                ", roof='" + roof + '\'' +
+                '}';
+    }
+
+    // Static Builder class
     public static class Builder {
         private int walls;
         private int doors;
@@ -14,7 +49,7 @@ class House {
 
         public Builder setWalls(int walls) {
             this.walls = walls;
-            return this;
+            return this; // returning the Builder instance to allow method chaining
         }
 
         public Builder setDoors(int doors) {
@@ -32,15 +67,21 @@ class House {
             return this;
         }
 
+        // Method to create a House instance
         public House build() {
             return new House(this);
         }
     }
 
-    private House(Builder builder) {
-        this.walls = builder.walls;
-        this.doors = builder.doors;
-        this.windows = builder.windows;
-        this.roof = builder.roof;
+    public static void main(String[] args) {
+        // Using the builder pattern to create a House object
+        House house = new House.Builder()
+                .setWalls(4)
+                .setDoors(2)
+                .setWindows(8)
+                .setRoof("Gabled")
+                .build();
+
+        System.out.println(house); // Output: House{walls=4, doors=2, windows=8, roof='Gabled'}
     }
 }
