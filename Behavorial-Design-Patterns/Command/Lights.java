92b6971cf7ed9b52419ea65956d71ca3ec928290
birunthabanr <birunthur@gmail.com>
2024-08-02 22:26:18 +0530

    command - onlights example

--- Behavorial-Design-Patterns/Command/Lights.java
+++ Behavorial-Design-Patterns/Command/Lights.java
@@ -15,27 +15,40 @@ class OnLights implements Order {
 }
 
 class Room{
-    protected OnLights lightCommand;
+    public Order lightCommand;
 
     public Room() {}
 
-    public void setCommand(OnLights lightCommand) {
+    public void setCommand(Order lightCommand) {
         this.lightCommand = lightCommand; 
     }
 
     public void executeCommand() {
-        lightCommand.execute()
+        lightCommand.execute();
     }
 }
 
 class Light {
-
+    private boolean switchedOn;
+    
+    public void switchLights() {
+        switchedOn = !switchedOn;
+    }
 }
 
 
 
 public class Lights {
     public static void main(String[] args) {
+
+        Light light = new Light();
+        Room room = new Room();
+
+        Order lightCommand = new  OnLights(light);
+        
+        room.setCommand(lightCommand);
+
+        room.executeCommand();
 
     }
     
