860720a5c0e4587d4118f8e093afb04649c2a428
birunthabanr <birunthur@gmail.com>
2024-08-03 09:20:41 +0530

    -

--- Behavorial-Design-Patterns/Observer/officeAuto.java
+++ Behavorial-Design-Patterns/Observer/officeAuto.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.*;
+
 interface listeners {
-    public void update();
+    public void update(String state);
 }
 
 class lightListener implements listeners {
@@ -15,6 +18,38 @@ class lightListener implements listeners {
     }
 }
 
+class lightSensors {
+    private List<listeners> observers = new ArrayList<>();
+    private String state;
+    private final int threshold;
+
+    public lightSensors(int threshold) {
+        this.threshold=threshold;
+    }
+
+    public void add(listeners listener) {
+        observers.add(listener);
+    }
+
+    public void remove(listeners listener) {
+        observers.remove(listener);
+    }
+
+    public void setLightLevel(String lightLevel) {
+        this.state = lightLevel;
+        notifyListeners();
+    }
+
+    public void notifyListeners(){
+        for (listeners observer : observers) {
+            observer.update(state);
+        }
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+}
 
 public class officeAuto {
     
